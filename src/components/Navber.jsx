b5d501a55dbfe680ff5b4fb6678e944809c41146
Shaiful191 <saifulislamridoy1999@gmail.com>
2024-12-28 12:11:07 +0600

    inline style as a variable

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
 const Navber = () => {
+  const style = {
+    color: 'white',
+    backgroundColor: '#3535f1',
+    borderRadius: '8px',
+  };
   return (
     <nav className="navbar">
       <h1>The Fundamental Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create" style={{
-          color: 'white',
-          backgroundColor: '#f1356d',
-          borderRadius: '8px',
-        }}>New Blog</a>
+        <a href="/create" style={style}>New Blog</a>
       </div>
     </nav>
   )
